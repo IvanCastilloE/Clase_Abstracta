159f89a36b0fd65f42d201def80bf6164565e969
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-02-15 13:45:26 -0700

    Agregar archivos de proyecto.

---
+++ ClaseAbstracta/EmpleadoNomina.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstracta
+{
+    class EmpleadoNomina: Empleado
+    {
+        public decimal SueldoBase { get; set; }
+
+        public override decimal Salario
+        {
+            get
+            {
+                return SueldoBase;
+            }
+        }
+
+        public EmpleadoNomina(string nombre, string puesto, decimal sueldoBase) : base(nombre, puesto)
+        {
+            SueldoBase = sueldoBase;
+        }
+        public override void Trabajar()
+        {
+            Console.WriteLine("Trabajando por la nomina");
+        }
+    }
+}
