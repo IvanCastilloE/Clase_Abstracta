19c6d23c3357b183328e1e850f1344e23297fbf6
Angel Ivan Castillo <angel_xayan06@hotmail.com>
2019-02-15 19:37:12 -0700

    Tarea

---
+++ ClaseAbstracta/ClaseAbstracta/Empleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstracta
+{
+    abstract class Empleado //Definimos la palabra reservada abstract
+    {
+        public string Nombre { get; set; }
+        public string Puesto { get; set; }
+        public abstract decimal Salario { get; } //Se calculará desde una subclase por eso es abstracta
+
+        public Empleado(string nombre, string puesto)
+        {
+            Nombre = nombre;
+            Puesto = puesto;
+        }
+
+        public void Saludar()
+        {
+            Console.WriteLine("Hola bb mi nombre es: {0} y mi puesto: {1}", Nombre, Puesto);
+        }
+
+        public abstract void Trabajar();
+    }
+}
---
+++ ClaseAbstracta/ClaseAbstracta/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstracta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpleadoHonorarios EH = new EmpleadoHonorarios("Ivan", "RH", 8000);
+            EH.Trabajar();
+            Console.WriteLine("Salario por honorarios: {0}", EH.Salario);
+            EmpleadoNomina EN = new EmpleadoNomina("Ivan", "Ventas", 5000);
+            EN.Trabajar();
+            Console.WriteLine("Salario por nomina: {0}", EN.Salario);
+
+            Console.ReadKey();
+        }
+    }
+}
